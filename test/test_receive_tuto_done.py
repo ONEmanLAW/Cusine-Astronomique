759016e8a6e0dcd63d20d04c77a96d8c0473bb86
ONEmanLAW <hugoculot.dev@gmail.com>
2026-01-28 23:44:14 +0100

    Added a working test for mad mapper sending message to vscode

---
+++ test/test_receive_tuto_done.py
@@ -0,0 +1,26 @@
+# test_receive_tuto_done.py
+# Reçoit le message OSC envoyé par MadMapper: /mm/tuto/done 1
+
+import logging
+from pythonosc.dispatcher import Dispatcher
+from pythonosc.osc_server import ThreadingOSCUDPServer
+
+LISTEN_IP = "0.0.0.0"
+LISTEN_PORT = 1234
+ADDR_DONE = "/mm/tuto/done"
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(message)s")
+
+def on_done(address, value):
+    logging.info(f"RECEIVED {address} {value}")
+
+def on_any(address, *args):
+    logging.info(f"IN {address} args={args}")
+
+disp = Dispatcher()
+disp.map(ADDR_DONE, on_done)
+disp.set_default_handler(on_any) 
+
+server = ThreadingOSCUDPServer((LISTEN_IP, LISTEN_PORT), disp)
+logging.info(f"Listening UDP {LISTEN_IP}:{LISTEN_PORT}")
+server.serve_forever()
